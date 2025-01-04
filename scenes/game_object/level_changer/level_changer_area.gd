618d7adba315c6cb2c95800e2caf07758940f8f4
Máximo Martínez <maximomzts@gmail.com>
2025-01-04 12:04:47 +0100

    Added the smith house to lobby

--- scenes/game_object/level_changer/level_changer_area.gd
+++ scenes/game_object/level_changer/level_changer_area.gd
@@ -6,6 +6,5 @@ func _ready():
 	area_entered.connect(on_area_entered)
 
 func on_area_entered(_area):
-	print("i entered")
 	MetaProgression.save()
 	ScreenTransition.transition_to_scene(level.resource_path)
