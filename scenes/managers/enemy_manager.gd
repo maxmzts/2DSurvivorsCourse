29d81cb8ceff9fd22bff903b946d04990c2b222d
Máximo Martínez <maximomzts@gmail.com>
2025-01-09 17:19:37 +0100

    More enemies spawn every minute (course)

--- scenes/managers/enemy_manager.gd
+++ scenes/managers/enemy_manager.gd
@@ -2,22 +2,35 @@ extends Node
 
 const SPAWN_RADIUS = 360
 
+@export_group("Main enemy 1")
 @export var basic_enemy_scene: PackedScene
+@export var basic_enemy_base_spawn_rate: int
+@export var basic_enemy_spawn_time: int
+@export_group("Main enemy 2")
 @export var wizard_enemy_scene: PackedScene
+@export var wizard_enemy_base_spawn_rate: int
+@export var wizard_enemy_spawn_time: int
+@export_group("Secondary enemy 1")
 @export var bat_enemy_scene: PackedScene
+@export var bat_enemy_base_spawn_rate: int
+@export var bat_enemy_spawn_time: int
+@export_group("Secondary enemy 2")
 @export var ghost_enemy_scene: PackedScene
+@export var ghost_enemy_base_spawn_rate: int
+@export var ghost_enemy_spawn_time: int
+
+@export_group("")
 @export var arena_time_manager: Node
 
 @onready var timer = $Timer
 
 var base_spawn_time = 0
-
 var enemy_table = WeightedTable.new()
+var spawn_count = 1
 
 func _ready():
 	base_spawn_time = timer.wait_time
 	enemy_table.add_item(basic_enemy_scene, 10)
-	enemy_table.add_item(ghost_enemy_scene, 10)
 	timer.timeout.connect(on_timer_timeout)
 	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
 
@@ -53,13 +66,14 @@ func on_timer_timeout():
 	if player == null:
 		return
 	
-	var enemy_scene = enemy_table.pick_item()
-	var enemy = enemy_scene.instantiate() as Node2D
-	
-	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
-	entities_layer.add_child(enemy)
-	enemy.global_position = get_spawn_position()
-	
+	for i in spawn_count:
+		var enemy_scene = enemy_table.pick_item()
+		var enemy = enemy_scene.instantiate() as Node2D
+		
+		var entities_layer = get_tree().get_first_node_in_group("entities_layer")
+		entities_layer.add_child(enemy)
+		enemy.global_position = get_spawn_position()
+
 
 func on_arena_difficulty_increased(arena_difficulty: int):
 	# the change of wait time wont be visible until the next restart of the timer
@@ -72,6 +86,9 @@ func on_arena_difficulty_increased(arena_difficulty: int):
 	if arena_difficulty == 8:
 		enemy_table.add_item(wizard_enemy_scene, 20)
 		enemy_table.increase_weight(bat_enemy_scene, 4)
-		enemy_table.add_item(ghost_enemy_scene, 2)
-
+		enemy_table.add_item(ghost_enemy_scene, 4)
+	
+	# spawns additional more enemy every 30 seconds
+	if (arena_difficulty % 12) == 0:
+		spawn_count += 1
 
